273dfcce793f5506698776fbdec59c9a78d6b8e9
Miguel Victor <migzwebhost@gmail.com>
2015-06-29 17:23:32 +0800

    Added favicon.ico shortcut in the safest way :)

--- vHub/urls.py
+++ vHub/urls.py
@@ -2,12 +2,14 @@ from django.conf.urls import include, url
 from django.contrib import admin
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.generic.base import RedirectView
 
 urlpatterns = [
 	url(r'^', include('accounts.urls', namespace='accounts')),
 	url(r'^', include('sitemap.urls', namespace='sitemap')),
 	url(r'^videos/', include('app.urls', namespace='videos')),
 	url(r'^admin/', include(admin.site.urls)),
+	url(r'^favicon\.ico$', RedirectView.as_view(url='/static/sitemap/favicon.ico')),
 ]
 
 if settings.DEBUG:
