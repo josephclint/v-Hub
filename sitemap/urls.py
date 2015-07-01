53de82335610a96f68a14eab807b280cf7f9d4e8
Elen <elenflores02@gmail.com>
2015-07-01 14:02:15 +0800

    incorporate design

--- sitemap/urls.py
+++ sitemap/urls.py
@@ -3,6 +3,7 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
+    url(r'^$', views.index, name='index'),
     url(r'^about/$', views.about, name='about'),
     url(r'^faq/$', views.faq, name='faq'),
     url(r'^tos/$', views.tos, name='tos'),
