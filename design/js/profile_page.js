c090f9d3d66b09a8a47f387c346bc3627a32351a
Greg Emmanuel Alenton <gremmanuelenton02@gmail.com>
2015-07-08 13:33:36 +0800

    added javascript for profile page

--- design/js/profile_page.js
+++ design/js/profile_page.js
@@ -1,6 +1,4 @@
-$( document ).ready(function() {
-    $("[rel='tooltip']").tooltip();    
- 
+$( document ).ready(function() {   
     $('.thumbnail').hover(
         function(){
             $(this).find('.caption').slideDown(450); //.fadeIn(250)
@@ -9,5 +7,13 @@ $( document ).ready(function() {
             $(this).find('.caption').slideUp(450); //.fadeOut(205)
         }
     );
- 
+ 	
+ 	$('.toggle').on('click', 
+ 		function() {
+ 			var target = $($(this).data('target'));
+ 			var isDisabled = target.prop('disabled');
+
+ 			target.prop('disabled', !isDisabled);
+ 		}
+ 	);
 });
