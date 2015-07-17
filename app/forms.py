ef09af2b69e48c735dd39a8b3c72501442a0c6b6
Miguel Victor <migzwebhost@gmail.com>
2015-07-17 09:33:28 +0800

    Modified Video Upload form

--- app/forms.py
+++ app/forms.py
@@ -4,8 +4,37 @@ from models import Video
 
 
 class VideoForm(forms.ModelForm):
-    video = forms.FileField(widget=forms.FileInput(attrs={'accept': 'video/*'}))
-
     class Meta:
         model = Video
-        exclude = ['owner', 'datetime_added', 'datetime_modified']
+        fields = ['video', 'title', 'description', 'categories', 'tags']
+        widgets = {
+        	'title': forms.TextInput(attrs={
+        		'id': 'up_vid_title',
+        		'class': 'form-control',
+        		'placeholder': 'Title',
+        	}),
+        	'description': forms.Textarea(attrs={
+        		'id': 'up_vid_des',
+        		'class': 'form-control',
+        		'placeholder': 'Description',
+        		'wrap': 'hard',
+        		'rows': 3,
+        	}),
+        	'categories': forms.SelectMultiple(attrs={
+        		'id': 'up_vid_cat',
+        		'class': 'form-control',
+        		'placeholder': 'Title',
+        	}),
+        	'tags': forms.TextInput(attrs={
+        		'id': 'tags_panel',
+        		'class': 'form-control',
+        		'placeholder': 'Tags',
+        	}),
+        	'video': forms.FileInput(attrs={
+				'accept': 'video/*'
+        	})
+        }
+
+    def save(self, commit=True):
+        self.data['tags'] = self.data['tags'].strip().split(";")
+        return super(VideoForm, self).save(commit=commit)
