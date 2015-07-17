b238a4ceedc5410b863c3be42840e21de015e198
Greg Emmanuel Alenton <gremmanuelenton02@gmail.com>
2015-07-17 15:46:01 +0800

    added upload page, modified profile and video

--- app/forms.py
+++ app/forms.py
@@ -8,6 +8,7 @@ class VideoForm(forms.ModelForm):
         model = Video
         fields = ['video', 'title', 'description', 'categories', 'tags']
         widgets = {
+<<<<<<< Updated upstream
             'title': forms.TextInput(attrs={
                 'id': 'up_vid_title',
                 'class': 'form-control',
@@ -33,6 +34,32 @@ class VideoForm(forms.ModelForm):
             'video': forms.FileInput(attrs={
                 'accept': 'video/*'
             })
+=======
+        	'title': forms.TextInput(attrs={
+        		'id': 'up_vid_title',
+        		'class': 'form-control',
+        		'placeholder': 'Title',
+        	}),
+        	'description': forms.Textarea(attrs={
+        		'id': 'up_vid_des',
+        		'class': 'form-control',
+        		'placeholder': 'Description',
+        		'wrap': 'hard',
+        		'rows': 3,
+        	}),
+        	'categories': forms.SelectMultiple(attrs={
+        		'id': 'up_vid_cat',
+        		'class': 'form-control',
+        	}),
+        	'tags': forms.TextInput(attrs={
+        		'id': 'tags_panel',
+        		'class': 'form-control',
+        		'placeholder': 'Tags',
+        	}),
+        	'video': forms.FileInput(attrs={
+				'accept': 'video/*'
+        	})
+>>>>>>> Stashed changes
         }
 
     def save(self, commit=True):
