2628d81f7547751b853fdc78015caef4bf6b6ea6
Miguel Victor <migzwebhost@gmail.com>
2015-07-17 16:27:41 +0800

    Changed tags so that it is optional. Added time field for comment

--- app/models.py
+++ app/models.py
@@ -27,7 +27,7 @@ class Video(models.Model):
     description = models.TextField()
     datetime_added = models.DateTimeField(auto_now_add=True)
     datetime_modified = models.DateTimeField(auto_now=True)
-    tags = models.ManyToManyField(Tag)
+    tags = models.ManyToManyField(Tag, blank=True)
     categories = models.ManyToManyField(Category)
 
     def __unicode__(self):
@@ -40,6 +40,9 @@ class Comment(models.Model):
     comment_text = models.TextField()
     likes = models.IntegerField(default=0)
     dislikes = models.IntegerField(default=0)
+    time = models.DecimalField()
+    datetime_added = models.DateTimeField(auto_now_add=True)
+    datetime_modified = models.DateTimeField(auto_now=True)
 
     def __unicode__(self):
         return self.comment_text
