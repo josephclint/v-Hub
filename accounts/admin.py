19572b6421e4d5681b9316de456758ff885e78c1
Virgil John A. Lomocso <vjlomocso@gmail.com>
2015-07-25 10:16:54 +0800

    Modified User in admin

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,4 +1,24 @@
 from django.contrib import admin
+from django.contrib.auth.models import User
+
 from models import UserProfile
-# Register your models here.
-admin.site.register(UserProfile)
+
+
+class UserProfileInline(admin.StackedInline):
+    model = UserProfile
+    can_delete = False
+    verbose_name_plural = 'Additional Information'
+
+
+class UserAdmin(admin.ModelAdmin):
+    fieldsets = (
+        ('Account Information', {'fields': ['username', 'email']}),
+        ('Personal Information', {'fields': ['first_name', 'last_name']})
+    )
+
+    ('username', 'password')
+    inlines = (UserProfileInline,)
+
+
+admin.site.unregister(User)
+admin.site.register(User, UserAdmin)
