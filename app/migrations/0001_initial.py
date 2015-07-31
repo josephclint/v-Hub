d96b27b025826bc782952ef030ad027e5ef39e94
Miguel Victor Remulta <migzwebhost@gmail.com>
2015-07-31 13:56:01 +0800

    Fixed migration incompatibilities

--- app/migrations/0001_initial.py
+++ app/migrations/0001_initial.py
@@ -32,6 +32,7 @@ class Migration(migrations.Migration):
             fields=[
                 ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
                 ('category_text', models.CharField(max_length=50)),
+                ('icon', models.CharField(max_length=255)),
             ],
             options={
                 'verbose_name_plural': 'categories',
